59afebf5a591a8cfe8862e53ef78eb8a3c515155
cyq <1324019229@qq.com>
2020-02-12 21:24:16 +0800

    用户信息修改

--- src/pages/admin-user.vue
+++ src/pages/admin-user.vue
@@ -50,7 +50,7 @@
           <template slot-scope="scope">
             <el-button
             size="mini"
-            @click="handleEdit(scope.$index, scope.row)">删除</el-button>
+            @click="handleDelet(scope.$index, scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -65,7 +65,6 @@
   </div>
 </template>
 <script>
-import dataTable from "@/components/_table.vue";
 import { getProductList } from "@/api";
 export default {
   name: 'HelloWorld',
@@ -102,69 +101,12 @@ export default {
         }]
     }
   },
-  name: "admin-user",
-  data() {
-    return {
-      columns: [
-        { field: "PKID", title: "隐藏主键", hidden: true },
-        { field: "name", title: "昵称" },
-        { field: "Phone", title: "联系电话" },
-        { field: "sex", title: "性别" },
-        { field: "birth", title: "出生日期" },
-        { field: "city", title: "所在城市" },
-        { field: "baduser", title: "拉黑用户" }
-      ],
-      // 页面有筛选时用到 查询表单
-      form: {
-        DistributorName: "",
-        FKGradeID: "",
-        CurrentStatus: "",
-        AuthStartTime: "",
-        AuthEndTime: "",
-        ExamineStatus: 2
-      },
-      // 分页、排序 控制器
-      pageing: {
-        url: "ApiUrl.PList",
-        total: 0,
-        sort: "PKID",
-        order: "DESC",
-        page: 1,
-        limit: 20
-      },
-      // 自定义按钮
-      handleArr: ["编辑", "启用", "删除"]
-    };
+  mounted() {
+
   },
-  mounted() {},
   methods: {
-    defuClick() {
-      this.$refs.table.getList();
-    },
-    handleCommand(title, data) {
-      switch (title) {
-        case "编辑":
-          this.EditAway(data);
-          break;
-        case "删除":
-          this.DeleteAway(data);
-          break;
-        case "启用":
-          this.StartAway(data);
-          break;
-      }
-    },
-    // 编辑
-    EditAway(data) {
-      this.$refs.table.getList();
-    },
-    // 删除
-    DeleteAway() {
-      console.log("删除");
-    },
-    // 启用
-    StartAway() {
-      console.log("启用");
+    handleDelet () {
+
     }
   }
 };
